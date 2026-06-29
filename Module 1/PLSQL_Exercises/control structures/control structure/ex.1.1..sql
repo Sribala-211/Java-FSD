56e87c552642d3514a032625371f6be4cd9b3963
Sribala-211 <sribalag005@gmail.com>
2026-06-29 19:15:50 +0530

    Add files via upload

---
+++ Module 1/PLSQL_Exercises/control structures/control structure/ex.1.1..sql
@@ -0,0 +1,12 @@
+BEGIN
+   FOR rec IN (SELECT customer_id, age, loan_interest_rate 
+               FROM customers) LOOP
+      IF rec.age > 60 THEN
+         UPDATE customers
+         SET loan_interest_rate = loan_interest_rate - 0.01
+         WHERE customer_id = rec.customer_id;
+      END IF;
+   END LOOP;
+   COMMIT;
+END;
+/
